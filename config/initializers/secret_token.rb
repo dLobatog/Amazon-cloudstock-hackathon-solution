e46a9d120b0b3965f1ea09ca7793f61e477571e6
Daniel Lobato Garcia <elobatocs at google.com>
2012-03-17 18:18:05 -0700

    Rails new with no ActiveRecord

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmazonCloudstock::Application.config.secret_token = '1623bd363ebdfb3c47fabe74fe8acdb3d7dfd32a3a3f3dd00158ba382fee03b7c985d97971b2fabc9f30082172940902e94e4bb0bf1a32df662ccd0f64e78ce1'
